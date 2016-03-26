3ec695c6988e7e72a5203c2b8a0c09675eab0fd9
João Paulo <Joo Paulo@JooPaulo-PC>
2016-03-26 15:05:08 -0300

    Modificação do Formulário de Novo Cliente.

--- forms/form_novoproduto.php
+++ forms/form_novoproduto.php
@@ -1,22 +1,34 @@
-<div class="modal fade" id="frm_novobanner" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+<div class="modal fade" id="frm_novoproduto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title centro" id="exampleModalLabel">Novo Banner</h4>
+                <h4 class="modal-title centro" id="exampleModalLabel">Novo Produto/Serviço</h4>
             </div>
             <div class="modal-body">
                 <form name="frm_novobanner" id="frm_novobanner" method="POST" action="#">
                     <div class="form-group">
-                        <label for="imagem" class="control-label">Insira a imagem a ser ultilizada:</label>
-                        <input type="file" class="form-control" id="imagem" name="imagem" placeholder="Escolha sua imagem..." required="">
-                    </div>
-                    <div class="form-group">
-                        <label for="titulo" class="control-label">Informe um titulo para este banner:</label>
+                        <label for="titulo" class="control-label">Descrição:</label>
                         <input type="text" name="titulo" class="form-control" id="titulo" required="">
                     </div>
                     <div class="form-group">
-                        <label for="descricao" class="control-label">Informe uma breve descrição:</label>
+                        <label for="titulo" class="control-label">Tipo:</label>
+                        <input type="text" name="titulo" class="form-control" id="titulo" required="">
+                        <section name="titulo" class="form-control" id="titulo" required="">
+                            <option value="1">PRODUTO</option>
+                            <option value="2">SERVIÇO</option>
+                        </section> 
+                    </div>
+                    <div class="form-group">
+                        <label for="titulo" class="control-label">Categoria:</label>
+                        <input type="text" name="titulo" class="form-control" id="titulo" required="">
+                        <section name="titulo" class="form-control" id="titulo" required="">
+                            <option value="1">PRODUTO</option>
+                            <option value="2">SERVIÇO</option>
+                        </section> 
+                    </div>
+                    <div class="form-group">
+                        <label for="descricao" class="control-label">Valor: </label>
                         <textarea name="descricao" class="form-control" id="descricao" required="" rows="8"></textarea>
                     </div>
                     <div class="modal-footer">
