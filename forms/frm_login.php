5307a892fac2db0ca2b47c0e7dd7cc1d3c12d20a
João Paulo <Joo Paulo@JoaoPaulo-PC>
2016-04-11 08:27:14 -0300

    Janela de login modificado.

--- forms/frm_login.php
+++ forms/frm_login.php
@@ -16,10 +16,12 @@
             </div><!-- /.login-logo -->
             <div class="container-fluid">
                 <div class="col-md-8">
-                    <img src="./imagens/vivo.png">
+                    <img src="./imagens/vivo.png" style="width: 200px; height: 200px; display: block; margin-left: auto; margin-right: auto;">
+
                 </div>
                 <div class="col-md-4 thumbnail" style="padding: 15px;">
-                    <p class="login-box-msg">Acesso ao WebMaxcel</p>
+                    <h3 style="text-align: center;">Acesso ao Websistema</h3>
+                    <hr>
                     <form name="login" method="POST" action="logar.php">
                         <div class="form-group has-feedback">
                             <input class="form-control" name="email" required="" placeholder="E-mail"/>
@@ -50,6 +52,37 @@
                         echo "</div>";
                     }
                     ?>
+                </div>
+                <div class="col-md-4 col-md-offset-8 thumbnail" style="padding: 15px;">
+                    <h3 style="text-align: center;">Acesso ao Webmail</h3>
+                    <hr>
+                    <form action="https://email.uolhost.com.br/auth" method="post">
+                        <input type="hidden" name="lang" value="">
+
+
+                        <input type="hidden" name="domain" value="grupomaxcel.com.br">
+                        <input type="hidden" name="redir_url" value="email.uolhost.com.br/grupomaxcel.com.br">
+
+                        <div class="form-group has-feedback">
+                            <input type="text" value="" id="login" name="login" maxlength="255" tabindex="2" class="form-control" placeholder="E-mail" required="">
+                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                        </div>
+
+                        <div class="form-group has-feedback">
+                            <input type="password" value="" id="password" name="password" maxlength="255" class="form-control" placeholder="Senha" required="">
+                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                        </div>
+
+                        <div class="wm-login-panel-dialog-msg ">
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-4 col-xs-offset-8">
+                                <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+                            </div><!-- /.col -->
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div><!-- /.login-box-body -->
